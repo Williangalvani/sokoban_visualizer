0d5ae840ab512c22915bb1356664e30e1db1d344
Willian Galvani <williangalvani@gmail.com>
2017-05-07 00:40:42 -0300

    v2

--- player.js
+++ player.js
@@ -2,81 +2,132 @@
 var moves = ["left", "right", "down", "up", "push left", "push right", "push down", "push up"];
 
 var fullsize = 350;
-var step = fullsize/5;
+var step = fullsize/7;
 
 
-var x = 0.5;
-var y = 0.5;
+var x = 1;
+var y = 1;
 
 var moveIndex = 0;
 var indexes = [];
+var matrix = null;
 
-var colors = ['#f00','#0f0','#00f','#550','#055','#505'];
+var colors = ['#fff','#000','#550','#555','#055','#505'];
 
 function choose(choices) {
   var index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
 
+var magicspots = [];
 
 
 
 function right()
 {
-console.log("right");
-// Draw the red line.
-canvasContext.beginPath();
-canvasContext.strokeStyle = choose(colors);
-canvasContext.moveTo(x*step, y*step);
-canvasContext.lineTo((x+1)*step, y*step);
-x = x + 1;
-canvasContext.stroke();	
+    if (matrix[y][x+1]==0)
+    {
+        matrix[y][x+1]=3;
+        matrix[y][x] = 0;   
+        x=x+1;
+    }
+    else if (matrix[y][x+1] == 2 && matrix[y][x+2] == 0)
+    {
+        matrix[y][x] = 0;
+        matrix[y][x+1]=3;
+        matrix[y][x+2]=2;  
+        x=x+1;
+    }else
+    {
+        alert("bad move?");
+    }
 
-};
+}
 
 function left()
 {
-	console.log("left");
-	// Draw the red line.
-canvasContext.beginPath();
-canvasContext.strokeStyle = choose(colors);
-canvasContext.moveTo(x*step, y*step);
-canvasContext.lineTo((x-1)*step, y*step);
-x = x - 1;
-canvasContext.stroke();	
-
-};
+    if (matrix[y][x-1]==0)
+    {
+        matrix[y][x-1]=3;
+        matrix[y][x] = 0;  
+        x=x-1; 
+    }
+    else if (matrix[y][x-1] == 2 && matrix[y][x-2] == 0)
+    {
+        matrix[y][x] = 0;
+        matrix[y][x-1]=3;
+        matrix[y][x-2]=2;  
+        x=x-1;
+    }else
+    {
+        alert("bad move?");
+    }
+}
 
 function up()
 {
-
-	console.log("up");
-	// Draw the red line.
-canvasContext.beginPath();
-canvasContext.strokeStyle = choose(colors);
-canvasContext.moveTo(x*step, y*step);
-canvasContext.lineTo(x*step, (y-1)*step);
-y = y - 1;
-canvasContext.stroke();	
-
-};
+    if (matrix[y-1][x]==0)
+    {
+        matrix[y-1][x]=3;
+        matrix[y][x] = 0;  
+        y=y-1; 
+    }
+    else if (matrix[y-1][x] == 2 && matrix[y-2][x] == 0)
+    {
+        matrix[y][x] = 0;
+        matrix[y-1][x]=3;
+        matrix[y-2][x]=2;  
+        y=y-1;
+    }else
+    {
+        alert("bad move?");
+    }
+}
 
 function down()
 {
+    if (matrix[y+1][x]==0)
+    {
+        matrix[y+1][x]=3;
+        matrix[y][x] = 0;
+        y=y+1;   
+    }
+    else if (matrix[y+1][x] == 2 && matrix[y+2][x] == 0)
+    {
+        matrix[y][x] = 0;
+        matrix[y+1][x]=3;
+        matrix[y+2][x]=2;  
+        y=y+1;
+    }else
+    {
+        alert("bad move?");
+    }
+}
 
 
-	console.log("down");
-	// Draw the red line.
-    canvasContext.beginPath();
-    canvasContext.strokeStyle = choose(colors);
-    canvasContext.moveTo(x*step, y*step);
-    canvasContext.lineTo(x*step, (y+1)*step);
-    y = y + 1;
-    console.log(y,x);
-    canvasContext.stroke();	
 
-};
-
+function drawMatrix()
+{
+    canvasContext.clearRect(0, 0, fullsize, fullsize);
+    for (var y_ = 0; y_<7 ;y_++)
+    {
+        for (var x_ = 0; x_<7 ;x_++)
+        {
+            canvasContext.fillStyle = colors[matrix[y_][x_]];
+            var xstart = 0 + x_*step;
+            var ystart = 0 + y_*step
+            canvasContext.fillRect(xstart,ystart,step,step);
+            canvasContext.fill();
+        }
+    }
+    for(var i=0; i<targets.length;i+=2)
+    {
+        tx = parseInt(targets[i+1])*step+10;
+        ty = parseInt(targets[i])*step+40;
+        canvasContext.font="30px Arial";
+        canvasContext.fillText("X",tx,ty);
+    }
+}
 
 
 var movesf = [left, right, up, down];
@@ -84,11 +135,14 @@ var movesf = [left, right, up, down];
 String.prototype.replaceAll = function(target, replacement) {
   return this.split(target).join(replacement);
 };
+
 function processData()
 {
+    targets = $('#targets').val().match(/[^[\]]+(?=])/g);
+    console.log(targets);
     eval($('#start'));
 	var text = $('#textInput').val();
-    var matrix = eval($('#Matrix').val());
+    matrix = eval($('#Matrix').val());
     console.log(matrix);
 	var splited = text.replaceAll("play_1_t","").replace(/ +(?= )/g,'').split(" ");
 	console.log(JSON.stringify(splited));
@@ -111,14 +165,26 @@ function processData()
         console.log(y, x);
 	}
 
-    var myVar = setInterval(function(){ myTimer() }, 500);
+    var myVar = setInterval(function(){ myTimer() }, 200);
 
     function myTimer() 
     {
+        try
+        {
 
-       var dir = (indexes[moveIndex]%4);
-       movesf[dir]();
-       moveIndex++;
+           var dir = (indexes[moveIndex]%4);
+           console.log(dir)
+           movesf[dir]();
+           moveIndex++;
+           drawMatrix();
+        }
+
+        catch(err)
+        {
+            console.log(err);   
+            clearInterval(myVar);
+           
+        }
     }
 
 
@@ -131,7 +197,7 @@ $('#textInput').bind('input propertychange', function() {
 
 
 
-
+/*
 function drawBoard(){
 for (var x = 0; x <= fullsize; x += step) {
     context.moveTo(0.5 + x + p, p);
@@ -148,7 +214,7 @@ context.strokeStyle = "black";
 context.stroke();
 }
 
-
+*/
 
 
 
@@ -159,7 +225,7 @@ canvas.width = 360;
 var canvasContext = canvas.getContext('2d');
 
 
-
+/*
     var bw = 350;
     var bh = 350;
     var p = 10;
@@ -183,6 +249,6 @@ var canvasContext = canvas.getContext('2d');
     }
 
     drawBoard();
-
+*/
 
 document.body.appendChild(canvas); 	
